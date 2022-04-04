642b2251896871ad32657fad8afd3dd49686ed34
ahmed-nasir <nasirahmed595@gmail.com>
2022-04-04 11:50:21 +0600

    add review card

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Review from '../Review/Review';
+import ReviewCard from '../ReviewCard/ReviewCard';
 
 
 const Home = () => {
@@ -13,26 +14,27 @@ const Home = () => {
                 // console.log(data);
                 setReviews(data);
             })
-    }, [reviews])
+    }, [])
 
 
     return (
         <div>
             <div className='grid grid-cols-1 md:grid-cols-2  items-center bg-orange-500 py-5 mb-3 '>
-                <div className='container flex justify-center px-36 '>
-                    <h1 className='text-white'><span className=' text-2xl md:text-5xl font-bold'>Hero text here Hero text here   </span> <br /> second linesecond linesecond line</h1>
+                <div className='container flex justify-center pl-32 '>
+                    <h1 className='text-white'><span className=' text-2xl md:text-5xl font-bold'>Revolutionary device at an unbelievable price.  </span> <br /> second linesecond linesecond line</h1>
                 </div>
                 <div className='flex justify-center'><img className=' md:h-[450px] md:w-[450px] h-60 w-60   md:object-cover' src="https://freepikpsd.com/file/2020/02/Macbook-Pro-PNG-Clipart-Free.png" /></div>
             </div>
             <div >
-                <div className='text-center'>
+                <div className='grid justify-center'>
                     <h1 className='text-4xl font-bold'>Customer Reviews</h1>
-                    <div>
+                    <div className='grid grid-cols-3 gap-3'>
                         {
-                            reviews?slice(0,3).map(review=><Review key={review.id} review={review}></Review>)
+                            // reviews?.slice(0, 3).map(review => <Review key={review.id} review={review}></Review>)
+                           reviews?.slice(0,3).map(review=><ReviewCard key={review.id} review={review}></ReviewCard>)
                         }
                     </div>
-                    <button className='bg-blue-600 rounded-sm p-2 text-white'>See More</button>
+                    <button className='bg-blue-600 rounded-sm p-2 text-white '>See More</button>
                 </div>
 
             </div>

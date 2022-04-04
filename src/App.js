e0cd6a6947166cb4acaaa1e9c0ac2cab84e1088a
ahmed-nasir <nasirahmed595@gmail.com>
2022-04-04 06:43:49 +0600

    Add react router dom & create route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
+import Deshboard from './components/Deshboard/Deshboard';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
-
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/deshboad' element={<Deshboard></Deshboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
     </>
   );
 }

792dda4dba967d44c1e3e3110d7b7659bb47603b
ahmed-nasir <nasirahmed595@gmail.com>
2022-04-05 13:31:36 +0600

    add redme file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import About from './components/About/About';
+
 import Blogs from './components/Blogs/Blogs';
 import Deshboard from './components/Deshboard/Deshboard';
 import Header from './components/Header/Header';
@@ -17,7 +17,6 @@ function App() {
         <Route path='/reviews' element={<Reviews></Reviews>}/>
         <Route path='/deshboad' element={<Deshboard></Deshboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
       </Routes>
     </>
--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Rating from 'react-rating';
 
 const ReviewCard = ({ review }) => {
-    console.log(review)
+    // console.log(review)
     const { name, picture, rating, review: reviewText } = review;
     return (
 
 
         <div className=" bg-gray-300 flex items-center  h-96 w-80 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <div className="flex flex-col  items-center ">
-                <img className="mb-3 w-24 h-24 rounded-full shadow-lg" src={review.picture} alt="Bonnieimage" />
+                <img className="mb-3 w-24 h-24 rounded-full shadow-lg" src={picture} alt="Bonnieimage" />
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
                 <span className="text-sm px-5 text-center text-gray-500 dark:text-gray-400">{reviewText.slice(0, 100)}...</span>
                 <div className="flex mt-4 space-x-3 lg:mt-6">
